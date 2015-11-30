cd0792af57af3f17c4d546e3602194ff60fa8e43
kkrzys <klimekkrzys@gmail.com>
2015-11-30 21:58:58 +0100

    possibility to add name to workflow

--- src/main/scala/pl/edu/agh/workflow/Workflow.scala
+++ src/main/scala/pl/edu/agh/workflow/Workflow.scala
@@ -1,11 +1,12 @@
 package pl.edu.agh.workflow
 
-class Workflow(block: => (List[Int], List[Int]) => List[Int]) extends AbstractWorkflow with Runnable {
+class Workflow(name: String, block: => (List[Int], List[Int]) => List[Int]) extends AbstractWorkflow with Runnable {
   def run: List[Int] = {
     block(in, out)
   }
 }
 
 object Workflow {
-  def apply(block: => (List[Int], List[Int]) => List[Int]) = new Workflow(block)
+  def apply(block: => (List[Int], List[Int]) => List[Int]) = new Workflow("", block)
+  def apply(name: String, block: => (List[Int], List[Int]) => List[Int]) = new Workflow(name, block)
 }

2082f7c258657d15d51ffd9df0e31dcc688f5310
kkrzys <klimekkrzys@gmail.com>
2016-09-18 15:05:59 +0200

    implemented getGroupedResultsAsync using FSM Sink actor

--- src/main/scala/pl/edu/agh/utils/SinkUtils.scala
+++ src/main/scala/pl/edu/agh/utils/SinkUtils.scala
@@ -12,47 +12,34 @@ import com.typesafe.config.ConfigFactory
 import pl.edu.agh.workflow.elements._
 
 object SinkUtils {
-  lazy val awaitTime = ConfigFactory.load().getInt("results.awaitTime")
+  lazy val awaitTime = ConfigFactory.load().getInt("sink.state.awaitTime")
 
-  def getResults[R](sink: ActorRef) = {
-    import pl.edu.agh.utils.ActorUtils._
-    import pl.edu.agh.utils.ActorUtils.Implicits._
-
-    val dataF = akka.pattern.after(awaitTime milliseconds, using = system.scheduler)(sink ? GetOut)
-    val data = Await.result(dataF, Duration.Inf).asInstanceOf[List[R]]
-
-    data
-  }
-  def getGroupedResults[R](sink: ActorRef)(size: Int) = {
-    import pl.edu.agh.utils.ActorUtils._
-    import pl.edu.agh.utils.ActorUtils.Implicits._
-
-    val dataF = akka.pattern.after(awaitTime milliseconds, using = system.scheduler)(sink ? GetGroupedOut(size))
-    val data = Await.result(dataF, Duration.Inf).asInstanceOf[Iterator[List[R]]]
-
-    data
-  }
   def getResultsAsync[R](sink: ActorRef): Future[List[R]] = {
+    import pl.edu.agh.utils.ActorUtils._
     import pl.edu.agh.utils.ActorUtils.Implicits._
 
+    def checkSinkState(sink: ActorRef, sinkActor: Sink[R]): Future[List[R]] = {
+      val res = Future.successful(sinkActor.stateName).flatMap {
+        case Idle => (sink ? GetOut).mapTo[List[R]]
+        case activeOrInit @ (Active | Init) =>
+          akka.pattern.after(awaitTime milliseconds, using = system.scheduler)(checkSinkState(sink, sinkActor))
+      }
+      res
+    }
     checkSinkState(sink, sink.toSinkActor)
   }
   def getGroupedResultsAsync[R](sink: ActorRef)(size: Int) = {
     import pl.edu.agh.utils.ActorUtils._
     import pl.edu.agh.utils.ActorUtils.Implicits._
 
-    val dataF = akka.pattern.after(awaitTime milliseconds, using = system.scheduler)(sink ? GetGroupedOut(size))
-    dataF.mapTo[Iterator[List[R]]]
-  }
-  private def checkSinkState[R](sink: ActorRef, sinkActor: Sink[R]): Future[List[R]] = {
-    import pl.edu.agh.utils.ActorUtils._
-    import pl.edu.agh.utils.ActorUtils.Implicits._
-
-    val res = Future.successful(sinkActor.stateName).flatMap {
-      case Idle => (sink ? GetOut).mapTo[List[R]]
-      case activeOrInit @ (Active | Init) =>
-        akka.pattern.after(200 milliseconds, using = system.scheduler)(checkSinkState(sink, sinkActor))
+    def checkSinkState(sink: ActorRef, sinkActor: Sink[R]): Future[Iterator[List[R]]] = {
+      val res = Future.successful(sinkActor.stateName).flatMap {
+        case Idle => (sink ? GetGroupedOut(size)).mapTo[Iterator[List[R]]]
+        case activeOrInit @ (Active | Init) =>
+          akka.pattern.after(awaitTime milliseconds, using = system.scheduler)(checkSinkState(sink, sinkActor))
+      }
+      res
     }
-    res
+    checkSinkState(sink, sink.toSinkActor)
   }
 }

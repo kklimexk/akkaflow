0457324e786b5e3427e413b47311f5c38c9f4915
kkrzys <klimekkrzys@gmail.com>
2016-09-23 21:18:16 +0200

    removed time after to checking if it is possibility to get results (awaitTime in application.conf)

--- src/main/scala/pl/edu/agh/utils/SinkUtils.scala
+++ src/main/scala/pl/edu/agh/utils/SinkUtils.scala
@@ -2,31 +2,28 @@ package pl.edu.agh.utils
 
 import akka.actor.ActorRef
 import akka.pattern.ask
-import com.typesafe.config.ConfigFactory
 import pl.edu.agh.messages.{GetGroupedOut, GetOut}
 import pl.edu.agh.utils.FSMStates._
 import pl.edu.agh.workflow.elements._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
-import scala.concurrent.duration._
 
 object SinkUtils {
-  lazy val awaitTime = ConfigFactory.load().getInt("config.state.awaitTime")
+  //lazy val awaitTime = ConfigFactory.load().getInt("config.state.awaitTime")
 
   def getResultsAsync[R](sink: ActorRef): Future[List[R]] = {
     import pl.edu.agh.utils.ActorUtils.Implicits._
-    import pl.edu.agh.utils.ActorUtils._
 
     def checkSinkState(sink: ActorRef, sinkActor: Sink[R]): Future[List[R]] = {
       val res = Future.successful(sinkActor.stateName).flatMap {
         case Idle => (sink ? GetOut).flatMap {
           case NotReady =>
-            akka.pattern.after(awaitTime milliseconds, using = system.scheduler)(checkSinkState(sink, sinkActor))
+            checkSinkState(sink, sinkActor)
           case result: List[R] => Future.successful(result)
         }
         case activeOrInit @ (Active | Init) =>
-          akka.pattern.after(awaitTime milliseconds, using = system.scheduler)(checkSinkState(sink, sinkActor))
+          checkSinkState(sink, sinkActor)
       }
       res
     }
@@ -34,17 +31,16 @@ object SinkUtils {
   }
   def getGroupedResultsAsync[R](sink: ActorRef)(size: Int) = {
     import pl.edu.agh.utils.ActorUtils.Implicits._
-    import pl.edu.agh.utils.ActorUtils._
 
     def checkSinkState(sink: ActorRef, sinkActor: Sink[R]): Future[Iterator[List[R]]] = {
       val res = Future.successful(sinkActor.stateName).flatMap {
         case Idle => (sink ? GetGroupedOut(size)).flatMap {
           case NotReady =>
-            akka.pattern.after(awaitTime milliseconds, using = system.scheduler)(checkSinkState(sink, sinkActor))
+            checkSinkState(sink, sinkActor)
           case result: Iterator[List[R]] => Future.successful(result)
         }
         case activeOrInit @ (Active | Init) =>
-          akka.pattern.after(awaitTime milliseconds, using = system.scheduler)(checkSinkState(sink, sinkActor))
+          checkSinkState(sink, sinkActor)
       }
       res
     }
